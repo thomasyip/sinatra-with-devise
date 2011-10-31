51b3dd17c5eca2802e1647b010437ea680be8264
Thomas @ BeeDesk <thomas@beedesk.com>
2011-10-31 16:38:12 -0700

    Added flash message.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  private
+
   before_filter :mailer_set_url_options
 
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
+  end
+ 
+  # The logged_in? method simply returns true if the user is logged
+  # in and false otherwise. It does this by "booleanizing" the
+  # current_user method we created previously using a double ! operator.
+  # Note that this is not common in Ruby and is discouraged unless you
+  # really mean to convert something into true or false.
+  def logged_in?
+    !!current_user
+  end
 
   def mailer_set_url_options
     ActionMailer::Base.default_url_options[:host] = request.host_with_port
   end
+
 end
