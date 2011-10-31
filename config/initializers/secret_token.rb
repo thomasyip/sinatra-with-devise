28cdc6dfbc9c1ddeaf767a3fe7e7600a7d1cf2d9
Thomas @ BeeDesk <thomas@beedesk.com>
2011-10-31 00:08:20 -0700

    Generated with 'rails new MyApp'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = 'a39a2a65c24d712453420c12ba16ac784cd35bf6e378e0e83d10cdac7aa089faa5be2bbdec29780f723d1c7d7b3d13b43eab9db7918cf9119fdc028fd1d1a87b'
