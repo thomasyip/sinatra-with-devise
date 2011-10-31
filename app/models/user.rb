25f33699a74728c4c13f94733db083841f31dcda
Thomas @ BeeDesk <thomas@beedesk.com>
2011-10-31 11:57:20 -0700

    Added first_name and last_name fields to model. Generated migration of adding names: 'rails generate migration add_name_to_users first_name:string last_name:string'.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
 end
